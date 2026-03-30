b88ccbdf1f23f92077df039b9be3d515c895243a
Lauren Chan <chanlc@umich.edu>
2026-03-30 09:33:28 +0200

    Add questions data and GET /api/questions

---
+++ data/questions.js
@@ -0,0 +1,64 @@
+const questions = [
+  {
+    id: 1,
+    question: "How many planets are in our solar system?",
+    answers: ["7", "8", "9", "10"],
+    correct: 1
+  },
+  {
+    id: 2,
+    question: "What is the capital city of Australia?",
+    answers: ["Sydney", "Melbourne", "Brisbane", "Canberra"],
+    correct: 3
+  },
+  {
+    id: 3,
+    question: "Which element has the chemical symbol 'Au'?",
+    answers: ["Silver", "Aluminium", "Gold", "Copper"],
+    correct: 2
+  },
+  {
+    id: 4,
+    question: "How many sides does a hexagon have?",
+    answers: ["5", "7", "8", "6"],
+    correct: 3
+  },
+  {
+    id: 5,
+    question: "Who painted the Mona Lisa?",
+    answers: ["Michelangelo", "Leonardo da Vinci", "Raphael", "Caravaggio"],
+    correct: 1
+  },
+  {
+    id: 6,
+    question: "What is the longest river in the world?",
+    answers: ["Amazon", "Yangtze", "Mississippi", "Nile"],
+    correct: 3
+  },
+  {
+    id: 7,
+    question: "How many strings does a standard guitar have?",
+    answers: ["4", "5", "6", "7"],
+    correct: 2
+  },
+  {
+    id: 8,
+    question: "What sport is played at Wimbledon?",
+    answers: ["Cricket", "Tennis", "Badminton", "Squash"],
+    correct: 1
+  },
+  {
+    id: 9,
+    question: "Which country is home to the kangaroo?",
+    answers: ["New Zealand", "South Africa", "Brazil", "Australia"],
+    correct: 3
+  },
+  {
+    id: 10,
+    question: "How many hours are in a week?",
+    answers: ["148", "156", "168", "172"],
+    correct: 2
+  }
+]
+
+module.exports = questions
